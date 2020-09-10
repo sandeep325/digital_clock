3fd50ed902c900b608a506a208c406502991155e
Sandeep325 <negisandeep9819@gmail.com>
2020-09-10 13:09:03 +0530

    Updates

---
+++ precache-manifest.87743cb850766689a800115502ac1a50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72eaddb775f52465fc28ef6e48616a6d",
+    "url": "/digital_clock/index.html"
+  },
+  {
+    "revision": "652c524e77e30bfb79bd",
+    "url": "/digital_clock/static/css/main.12c9c7c2.chunk.css"
+  },
+  {
+    "revision": "ed035d29c1ac1c2d9892",
+    "url": "/digital_clock/static/js/2.e6bc5cf0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digital_clock/static/js/2.e6bc5cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "652c524e77e30bfb79bd",
+    "url": "/digital_clock/static/js/main.5480c3ed.chunk.js"
+  },
+  {
+    "revision": "5edc4a7d937807359c16",
+    "url": "/digital_clock/static/js/runtime-main.d45e561c.js"
+  }
+]);
