2447689a1998a70fb18a225460800215b1b74066
Sandeep325 <negisandeep9819@gmail.com>
2020-09-10 12:40:51 +0530

    digital commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App=()=>
+{
+  const newtime= new Date().toLocaleTimeString();
+   const[ctime, setCtime]=useState(newtime);
+
+
+ const today= new Date().toLocaleString("default", { weekday: "long" });
+ const[cday, setCday]=useState(today);
+
+   const Updatetime=()=>
+   {
+
+    let time= new Date().toLocaleTimeString();
+    setCtime(time);
+
+
+   let  Ntoday= new Date().toLocaleString("default", { weekday: "long" });
+    setCday(Ntoday);
+
+   };
+   setInterval(Updatetime,1000)
+ return(
+     <>
+       <h1>{ctime}</h1>
+       <h2>{cday}</h2>
+       <h5>By Sandeep</h5>
+     </>
+     );
+
 }
-
 export default App;
